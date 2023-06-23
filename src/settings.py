c036b8a25f814dbc01043386dd4d2c3d558cefb1
Davide Stallone <stlo@zhaw.ch>
2023-06-23 16:36:12 +0200

    Fix BATCH_INTERVAL default value

--- src/settings.py
+++ src/settings.py
@@ -5,7 +5,7 @@ _settings = {
     "PROJECT_DB_API_KEY": os.getenv("PROJECT_DB_API_KEY", ""),
     "DB_HOST": os.getenv("DB_HOST", "localhost:8080"),
     "IMPORT_INTERVAL": int(os.getenv('IMPORT_INTERVAL', 86400)), # 24h
-    "BATCH_INTERVAL": int(os.getenv('BATCH_INTERVAL', 3600)), # 5 min
+    "BATCH_INTERVAL": int(os.getenv('BATCH_INTERVAL', 300)), # 5 min
     "BATCH_SIZE": int(os.getenv("BATCH_SIZE", 100)),
     "LOG_LEVEL": os.getenv("LOG_LEVEL", "ERROR"),
     "MQ_HOST": "mq",
